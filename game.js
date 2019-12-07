5384bd5541bafdd5908b34fb77f1426587d1fdf1
connerm2015 <58260938+connerm2015@users.noreply.github.com>
2019-12-07 10:05:31 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,31 +1,20 @@
-var cvs = document.getElementById("canvas");
-var ctx = cvs.getContext("2d");
-
-// load images
-
-var bird = new Image();
-var bg = new Image();
-var fg = new Image();
-var pipeNorth = new Image();
-var pipeSouth = new Image();
-
-bird.src = "images/turtle.png";
-bg.src = "images/bg.png";
-pipeNorth.src = "images/shark.png";
-pipeSouth.src = "images/jellyfish.png";
-
-
-// some variables
-
-var gap = 300;
-var constant;
-
+var ctx;
+var imgBg;
+var imgDrops;
+var x = 0;
+var y = 0;
+var amt = 2;
+var enemy = [];
 var bX = 10;
 var bY = 150;
-
-var gravity = 1.5;
-
+var gravity = 2;
 var score = 0;
+
+//images
+imgBg = new Image();
+var turtle = new Image();
+
+turtle.src = "images/turtle.png";
 
 // audio files
 var up = false;
@@ -39,77 +28,73 @@ var scor = new Audio();
 fly.src = "sounds/fly.mp3";
 scor.src = "sounds/score.mp3";
 
-// on key down
-
+//move up
 document.addEventListener("keydown",moveUp);
 
 function moveUp(){
-    bY -= 25;
+    bY -= 35;
     fly.play();
 }
 
-// pipe coordinates
 
-var pipe = [];
+function drawBackground(){  
+    ctx.drawImage(imgBg, 0, 0); //Background
+}
 
-pipe[0] = {
-    x : cvs.width,
-    y : 0
-};
+function draw() {
+    drawBackground();
+    //draws the turtle to canvas
+    ctx.drawImage(turtle,bX,bY);
 
 
 
-// draw images
-
-function draw(){
-    
-    ctx.drawImage(bg,0,0);
-    
-    
-    for(var i = 0; i < pipe.length; i++){
+    for (var i=0; i < amt; i++){
         
-        constant = pipeNorth.height+gap;
-        ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
-        ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);
-             
-        pipe[i].x--;
-        
-        if( pipe[i].x == 125 ){
-            pipe.push({
-                x : cvs.width,
-                y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height
-            }); 
+        ctx.drawImage (enemy[i].image, enemy[i].x, enemy[i].y); //The rain drop
+
+        enemy[i].x += enemy[i].speed; //Set the falling speed
+            
+        if (enemy[i].x > 500)  {  //Repeat the raindrop when it falls out of view
+            enemy[i].x = -25 //Account for the image size
+            enemy[i].y = Math.random() * 600;    //Make it appear randomly along the width    
         }
 
-        // detect collision
-        
-        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
-            location.reload(); // reload the page
-        }
-        
-        if(pipe[i].x == 5){
-            score++;
-            scor.play();
-        }
-        
-        
-    }
+    }  
 
-    ctx.drawImage(fg,0,cvs.height - fg.height);
-    
-    ctx.drawImage(bird,bX,bY);
-    
     bY += gravity;
+
+    // //score count
+    // ctx.fillStyle = "#000";
+    // ctx.font = "20px Verdana";
+    // ctx.fillText("Score : "+score,10,cvs.height-20);
     
-    ctx.fillStyle = "#000";
-    ctx.font = "20px Verdana";
-    ctx.fillText("Score : "+score,10,cvs.height-20);
-    
-    requestAnimationFrame(draw);
+    // requestAnimationFrame(draw);
     
 }
 
-draw();
+function setup() {
+    var canvas = document.getElementById('canvas');
+
+    if (canvas.getContext) {
+        ctx = canvas.getContext('2d');
+        
+        imgBg.src = "images/bg.png";
+        setInterval(draw, 40);
+        for (var i = 0; i < amt; i++) {
+            var fallingDr = new Object();
+            fallingDr["image"] =  new Image();
+            fallingDr.image.src = 'images/shark.png';
+            //falls to random points on x and y axis
+            fallingDr["x"] = Math.random() * 20;
+            fallingDr["y"] = Math.random() * 1000;
+            fallingDr["speed"] = 3 + Math.random() *5;
+            enemy.push(fallingDr);
+        }
+
+    }
+}
 
 
 
+
+setup();
